35c442cd53aaecbd40c8bcc765ce98031068045f
Ron Mason <WHERESRONALD0@GMAIL.COM>
2018-10-02 10:02:13 -0400

    added show route

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ var app = express();
 var request = require("request");
 var mongoose = require("mongoose");
 
+//App Config//
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true })); //use all the time cut paste
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+//Mongoose Model Config//
 mongoose.connect(
   "mongodb://localhost/blogApp",
   { useNewUrlParser: true }
@@ -17,11 +19,65 @@ var blogSchema = mongoose.Schema({
   title: String,
   image: String,
   body: String,
-  created: Date
+  created: { type: Date, default: Date.now } //default language
 });
 
-var Blog = mongoose.model("blog", blogSchema);
+var Blog = mongoose.model("Blog", blogSchema);
+
+// Blog.create({
+//   title: "test blog",
+//   image:
+//     "https://images.pexels.com/photos/7316/food-purple-chocolate-dessert.jpg?auto=compress&cs=tinysrgb&h=650&w=940",
+//   body: "test tex about the body of this test blog post"
+// });
+
+//Routes//
+app.get("/", function(req, res) {
+  res.redirect("/blogs");
+});
+
+//INDEX ROUTE//
+app.get("/blogs", function(req, res) {
+  Blog.find({}, function(err, blogs) {
+    if (err) {
+      console.log("error!");
+    } else {
+      res.render("index", { blogs: blogs });
+    }
+  });
+});
+
+//NEW ROUTE
+app.get("/new", function(req, res) {
+  res.render("new");
+});
+
+//CREATE ROUTE
+app.post("/blogs", function(req, res) {
+  //create blog
+  Blog.create(req.body.blog, function(err, newBlog) {
+    if (err) {
+      res.render("new");
+    } else {
+      //then redirect to the index page
+      res.redirect("/blogs");
+    }
+  });
+});
+
+// //SHOW ROUTE
+app.get("/blogs/:id", function(req, res) {
+  Blog.findById(req.params.id, function(err, foundBlog) {
+    if (err) {
+      res.redirect("/blogs");
+    } else {
+      res.render("show", { blog: foundBlog });
+    }
+  });
+});
 
 app.listen(4000, function() {
   console.log("Blog server is fired up!!");
 });
+
+// <a href="/blogs/<%= blog._id%>">Read More</a>
---
+++ programming_note.md
@@ -0,0 +1,3 @@
+- <p><%=blog.body.substring(0, 100)%> ...</p> substring method on index.ejs limits the nubmer of charecters (this is used to truncate the kist view as not to show too much info on the index page)
+- <%-blog.body%> the dash instead of equals will allow users to input html in the description box- it evaluates the html and renders- theres an express santitizing package that will scrub out all the script tags so users can't run JS
+- look of moments.js for date display flexibilitiy
